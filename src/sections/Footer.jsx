89d9b780ec31531cacb78a85fcec9a5a866decbe
hsu win latt <lareina1467@gmail.com>
2025-03-25 04:15:27 +0700

    fix: footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -3,30 +3,23 @@ import { socialImgs } from "../constants";
 
 const Footer = () => {
   return (
-    <div className="flex-center my-10 text-white-50 xl:px-0 padding-x">
-      <div className="container grid-3-cols-md-tight">
-        <div className="flex flex-col justify-center">
-          <p className="md:text-start text-center cursor-pointer">
-            Terms & Conditions
-          </p>
+    <footer className="footer">
+      <div className="container">
+        <div className="left">
+          <p>Terms & Conditions</p>
         </div>
-        <div className="flex justify-center items-center gap-5">
+        <div className="socials">
           {socialImgs.map((socialImg, index) => (
-            <div
-              key={index}
-              className="common-card md:size-12 size-10 flex-center rounded-xl"
-            >
-              <img src={socialImg.imgPath} alt="icons" />
+            <div key={index} className="icon">
+              <img src={socialImg.imgPath} alt="social icon" />
             </div>
           ))}
         </div>
-        <div className="flex flex-col justify-center">
-          <p className="md:text-end text-center">
-            © 2024 Adrian Hajdin. All rights reserved.
-          </p>
+        <div className="right">
+          <p>© 2024 Adrian Hajdin. All rights reserved.</p>
         </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
