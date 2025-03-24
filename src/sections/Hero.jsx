0465288a9e9766a1d96b1ba1c0cb0219b2690e80
hsu win latt <lareina1467@gmail.com>
2025-03-24 18:52:43 +0700

    feat: hero section animate

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,8 +2,27 @@ import React from "react";
 import HeroExperience from "../components/models/HeroExperience";
 import AnimatedCounter from "../components/AnimatedCounter";
 import Button from "../components/Button";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".hero-text",
+      {
+        y: 50,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.1,
+        duration: 1,
+        ease: "power2.inOut",
+      }
+    );
+  });
+
   return (
     <section id="/">
       <div className="absolute top-0 left-0 z-10">
@@ -17,15 +36,17 @@ const Hero = () => {
             </div>
             <div className="xl:text-6xl text-3xl font-semibold xl:w-[33vw] xl:leading-20">
               <h1>
-                Transform
-                <span className="inline-block mx-3 overflow-hidden">
+                <span className="hero-text">Transform</span>
+                <span className="inline-block mx-3 overflow-hidden hero-text">
                   <img
                     src="/images/person.png"
                     alt="person"
                     className="size-5 md:size-10 object-contain"
                   />
                 </span>
-                Ideas into Seamless Digital Experiences
+                <span className="hero-text">
+                  Ideas into Seamless Digital Experiences
+                </span>
               </h1>
             </div>
             <p className="text-white-50 md:text-xl">
