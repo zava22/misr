9d2bdcbdb201a46de9c0a3c8e47b31da916a4fb5
hsu win latt <lareina1467@gmail.com>
2025-03-25 08:00:45 +0700

    chore: add cursor pointer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const Button = ({ text }) => {
   return (
-    <div className="cta-button group">
-      <div className="bg-circle" />
-      <p className="text">{text}</p>
-      <div className="arrow-wrapper">
-        <img src="/images/arrow-right.svg" alt="arrow" className="arrow" />
+    <div className="w-fit cursor-pointer">
+      <div className="cta-button group ">
+        <div className="bg-circle" />
+        <p className="text">{text}</p>
+        <div className="arrow-wrapper">
+          <img src="/images/arrow-right.svg" alt="arrow" className="arrow" />
+        </div>
       </div>
     </div>
   );
