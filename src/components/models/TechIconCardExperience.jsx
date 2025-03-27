eccda24b519f79d1ee29c4a30e6f36c4998bebdc
hsu win latt <lareina1467@gmail.com>
2025-03-27 16:18:16 +0700

    chore: change the color of t he Three.js model

--- src/components/models/TechIconCardExperience.jsx
+++ src/components/models/TechIconCardExperience.jsx
@@ -1,8 +1,22 @@
 import { Environment, Float, OrbitControls, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useEffect } from "react";
+import * as THREE from "three";
 
 const TechIconCardExperience = ({ model }) => {
   const scene = useGLTF(model.modelPath);
+
+  useEffect(() => {
+    if (model.name === "Interactive Developer") {
+      scene.scene.traverse((child) => {
+        if (child.isMesh) {
+          if (child.name === "Object_5") {
+            child.material = new THREE.MeshStandardMaterial({ color: "white" });
+          }
+        }
+      });
+    }
+  }, [scene]);
 
   return (
     <Canvas>
