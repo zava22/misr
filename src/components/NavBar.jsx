04a693fd838049d35e458138a04c7306e3c68fcb
hsu win latt <lareina1467@gmail.com>
2025-04-02 11:38:55 +0700

    chore: navbar mobile ui

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { navLinks } from "../constants";
 
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
 
   // Handle scroll effect
@@ -40,28 +39,6 @@ export default function Navbar() {
             <span>Contact me</span>
           </div>
         </a>
-
-        <button className="mobile-toggle" onClick={() => setIsOpen(!isOpen)}>
-          <img src={`/images/${isOpen ? "x" : "menu"}.svg`} alt="" />
-        </button>
-      </div>
-
-      <div className={`mobile-menu ${isOpen ? "open" : "closed"}`}>
-        <ul>
-          {navLinks.map((item, index) => (
-            <li key={index} className="group">
-              <a href={item.link} onClick={() => setIsOpen(false)}>
-                <span>{item.name}</span>
-                <span className="underline" />
-              </a>
-            </li>
-          ))}
-          <li>
-            <button className="cta" onClick={() => setIsOpen(false)}>
-              Contact me
-            </button>
-          </li>
-        </ul>
       </div>
     </header>
   );
