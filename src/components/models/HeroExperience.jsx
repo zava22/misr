c40f3f0e3114817b42a2ac7741b990f762d10e0c
hsu win latt <lareina1467@gmail.com>
2025-04-02 13:32:22 +0700

    feat: hero section model

--- src/components/models/HeroExperience.jsx
+++ src/components/models/HeroExperience.jsx
@@ -1,43 +1,52 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Room } from "./Room";
 import * as THREE from "three";
+import { Model } from "./Optimized-room";
 
 const HeroExperience = () => {
   return (
-    <Canvas camera={{ position: [0, 0, 15], fov: 45 }} shadows>
-      <OrbitControls />
-      <ambientLight intensity={0.3} color="#ffffff" />
+    <Canvas
+      camera={{ position: [0, 0, 15], fov: 45 }}
+      gl={{
+        toneMapping: THREE.ACESFilmicToneMapping,
+        outputEncoding: THREE.sRGBEncoding,
+      }}
+      shadows
+    >
+      <OrbitControls
+        enablePan={false}
+        maxDistance={20}
+        minPolarAngle={Math.PI / 5}
+        maxPolarAngle={Math.PI / 2}
+      />
 
-      <directionalLight position={[0, 7, 0]} intensity={1.5} color="#780000" />
+      <ambientLight intensity={0.5} color={"blue"} />
 
+      {/* lamp's light */}
       <spotLight
-        position={[0, 15, 0]}
-        angle={0.4}
-        decay={0.5}
-        penumbra={0.9}
-        intensity={50}
-        color="#0466c8"
-        castShadow
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
+        position={[4, 5, 4]}
+        angle={0.15}
+        penumbra={0.2}
+        intensity={100}
+        color="white"
       />
 
       <spotLight
-        position={[0.1, 0.2, 0.3]}
-        angle={0.7}
-        decay={0.5}
-        penumbra={0.9}
-        intensity={10}
-        color="#7209b7"
-        castShadow
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
+        position={[-3, 5, 5]}
+        angle={30}
+        penumbra={1}
+        intensity={150}
+        color="#ffb199"
       />
 
-      {/* Room geometry group */}
-      <group position={[0, -3.5, 0]} rotation={[0, -Math.PI / 4, 0]}>
-        <Room />
+      <pointLight position={[0, 1, 0]} intensity={30} color="#4cc9f0" />
+
+      <group
+        scale={1.3}
+        position={[0, -3.5, 0]}
+        rotation={[0, -Math.PI / 4, 0]}
+      >
+        <Model />
       </group>
     </Canvas>
   );
